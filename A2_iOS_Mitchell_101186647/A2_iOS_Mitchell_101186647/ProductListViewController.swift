1d8312b24355441167c04c33eb19eea3064be783
Mitchell Stevenson <101186647@georgebrown.ca>
2026-04-09 18:45:53 -0400

    Add product fetch

--- A2_iOS_Mitchell_101186647/A2_iOS_Mitchell_101186647/ProductListViewController.swift
+++ A2_iOS_Mitchell_101186647/A2_iOS_Mitchell_101186647/ProductListViewController.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import UIKit
+import CoreData
+
+class ProductListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var context: NSManagedObjectContext!
+    var products: [Product] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        fetchProducts()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchProducts()
+    }
+
+    func fetchProducts() {
+        let request: NSFetchRequest<Product> = Product.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "productID", ascending: true)]
+
+        do {
+            products = try context.fetch(request)
+            tableView.reloadData()
+        } catch {
+            print("Error fetching all products: \(error)")
+        }
+    }
+
+    
+}
